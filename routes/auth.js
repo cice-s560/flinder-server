76016e8cb56d8e8e9a89c1a5fa06539f6f636f0a
GonzaloCedillo <goncedillo@gmail.com>
2019-01-19 11:43:27 +0100

    Extracción de estrategia local de Passport

--- routes/auth.js
+++ routes/auth.js
@@ -4,82 +4,16 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require("bcrypt");
 const UserModel = require("../models/User");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+const localStrategy = require("../lib/strategies/local");
+
 
 /////////////////////////////////////////////////////////////
 ///// PASSPORT
-// passport.serializeUser(function(user, done) {
-//   done(null, user.id);
-// });
+passport.use(localStrategy);
 
-// passport.deserializeUser(function(id, done) {
-//   UserModel.findById(id, function(err, user) {
-//     done(err, user._id);
-//   });
-// });
-passport.use(
-  new LocalStrategy(function(username, password, done) {
-    UserModel.findOne({ "info.username": username }, async function(err, user) {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: "Incorrect username." });
-      }
-
-      const isMatchingPassword = await validateUserPassword(
-        user.auth.pass,
-        password
-      );
-
-      if (!isMatchingPassword) {
-        return done(null, false, { message: "Incorrect password." });
-      }
-
-      return done(null, { id: user._id, username: user.info.username });
-    });
-  })
-);
 
 /////////////////////////////////////////////////////////////
 ////// UTILIDADES
-
-function bcryptCompare(req, res, userdb) {
-  bcrypt.compare(req.body.pass.toString(), userdb.auth.pass, (err, result) => {
-    if (err) {
-      throw err;
-    }
-
-    if (result) {
-      req.session.user = userdb._id;
-    }
-
-    res.status(200).send("Login successful");
-  });
-}
-
-async function mongoFind(req, res) {
-  const filters = {
-    "info.username": req.body.username
-  };
-  const users = await UserModel.find(filters).catch(err =>
-    res.status(400).send("Error on user")
-  );
-  if (users === null) res.status(404).send("User not found");
-  bcryptCompare(req, res, users);
-}
-
-async function mongoFindOne(req, res) {
-  const filters = {
-    "info.username": req.body.username
-  };
-
-  const users = await UserModel.findOne(filters).catch(err =>
-    res.status(400).send("Error on user")
-  );
-  if (users === null) res.status(404).send("User not found");
-  bcryptCompare(req, res, users);
-}
 
 async function createHashedPassword(password) {
   return new Promise((resolve, reject) => {
@@ -92,13 +26,6 @@ async function createHashedPassword(password) {
       return resolve(hash);
     });
   });
-}
-
-async function validateUserPassword(userPassword, password) {
-  //   return new Promise((resolve, reject) => {
-  //     return bcrypt.compare(password, userPassword);
-  //   });
-  return bcrypt.compare(password, userPassword);
 }
 
 /////////////////////////////////////////////////////////////
