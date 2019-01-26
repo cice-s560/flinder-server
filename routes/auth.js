4a1fe7c98cc6563441bf110df7680331a9962531
perezm.josea <perezm.josea@gmail.com>
2019-01-26 09:36:47 +0100

    Adding Spotify Auth Strategy

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,18 @@
 var express = require("express");
 var router = express.Router();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const UserModel = require("../models/User");
 const passport = require("passport");
 const localStrategy = require("../lib/strategies/local");
 const googleStrategy = require("../lib/strategies/google");
-
+const spotifyStrategy = require("../lib/strategies/spotify");
 
 /////////////////////////////////////////////////////////////
 ///// PASSPORT
 passport.use(localStrategy);
 passport.use(googleStrategy);
-
+passport.use(spotifyStrategy);
 
 /////////////////////////////////////////////////////////////
 ////// UTILIDADES
@@ -62,34 +62,80 @@ router.post("/signup", async (req, res) => {
   user.auth.pass = await createHashedPassword(req.body.password);
 
   await user.save().catch(err => {
-    throw err;
-    return res.status(500).send();
+    // throw err;
+    return res.status(401).send({ Error: err });
   });
 
   return res.status(201).send("created successfully");
 });
 
-router.post("/", passport.authenticate("local", { session: false }), async function (req, res) {
-  // Creo el token con un payload que representa el obj que Passport me da al autorizar
-  try {
-    const token = await generateJWT(req.user);
+router.post(
+  "/",
+  passport.authenticate("local", { session: false }),
+  async function(req, res) {
+    // Creo el token con un payload que representa el obj que Passport me da al autorizar
+    try {
+      const token = await generateJWT(req.user);
 
-    return res.status(200).json({ token });
-  } catch (err) {
-    return res.status(500).json({ error: "JWT Fails" });
+      return res.status(200).json({ token });
+    } catch (err) {
+      return res.status(500).json({ error: "JWT Fails" });
+    }
   }
-});
+);
 
-router.get("/signin/google", passport.authorize("google", { scope: ["email", "profile"] }));
+router.get(
+  "/signin/google",
+  passport.authorize("google", { scope: ["email", "profile"] })
+);
 
-router.get("/callback/google", passport.authenticate("google", { session: false }), async (req, res) => {
-  try {
-    const token = await generateJWT(req.user);
+router.get(
+  "/callback/google",
+  passport.authenticate("google", { session: false }),
+  async (req, res) => {
+    try {
+      const token = await generateJWT(req.user);
 
-    return res.redirect(`${process.env.CLIENT_AUTH_CALLBACK_URL}?token=${token}`);
-  } catch (err) {
-    return res.redirect(process.env.CLIENT_AUTH_CALLBACK_FAILS);
+      return res.redirect(
+        `${process.env.CLIENT_AUTH_CALLBACK_URL}?token=${token}`
+      );
+    } catch (err) {
+      return res.redirect(process.env.CLIENT_AUTH_CALLBACK_FAILS);
+    }
   }
-})
+);
+
+router.get(
+  "/signin/spotify",
+  passport.authorize("spotify", {
+    scope: ["user-read-private", " user-read-email"]
+  })
+);
+
+router.get(
+  "/callback/spotify",
+  passport.authenticate("spotify", { session: false }),
+  async (req, res) => {
+    try {
+      const token = await generateJWT(req.user);
+      return res.redirect(
+        `${process.env.CLIENT_AUTH_CALLBACK_URL}?token=${token}`
+      );
+    } catch (err) {
+      return res.redirect(process.env.CLIENT_AUTH_CALLBACK_FAILS);
+    }
+  }
+
+  // passport.authenticate("spotify", {
+  //   failureRedirect: process.env.CLIENT_AUTH_CALLBACK_FAILS
+  // }),
+  // async (req, res) => {
+  //   // Successful authentication, redirect home.
+  //   const token = await generateJWT(req.user);
+  //   return res.redirect(
+  //     `${process.env.CLIENT_AUTH_CALLBACK_URL}?token=${token}`
+  //   );
+  // }
+);
 
 module.exports = router;
