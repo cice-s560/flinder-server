d977e8ff3d538c55df6dacb0f75070ee43146150
Roberto <rob.vicalvaro@gmail.com>
2019-02-17 16:16:05 +0100

    mas merge conflicts

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const chalk = require('chalk');
 const emoji = require('node-emoji');
 
-<<<<<<< HEAD
 
 const port = process.env.PORT || 3001;
 const db_port = process.env.DB_PORT || 27018;
@@ -22,19 +21,6 @@ server.listen(port, () => {
        console.info(chalk.black.bgMagenta(`${emoji.get('heart')}   WELLCOME TO FLINDER! ${emoji.get('heart')}  
  running at ${port}    
  Good luck! and remember: ${emoji.get('coffee')}   + ${emoji.get('smoking')}  = ${emoji.get('poop')}  `))
-=======
-const port = process.env.PORT || 3001;
-const dbURL = process.env.DB_URL || "localhost";
-const dbPort = process.env.DB_PORT || 27018;
-const dbName = process.env.DB_NAME || "basic_auth";
 
-server.listen(port, () => {
-    mongoose.connect(`mongodb://${dbURL}:${dbPort}/${dbName}`, {useNewUrlParser: true}, err => {
-       if (err) {
-           throw err;
-       }
-
-       console.log(`API running at port ${port}`);
->>>>>>> d76d084300729185a092b9bcdc82cae7ba022965
     });
 });
--- routes/check.js
+++ routes/check.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
-const checkToken = require("../lib/checktkn");
 
-
-router.get('/', checkToken, (req, res, next) => {
-    return res.json({username: req.user.username});
-=======
 
 async function checkSession(req, res, next) {
     if (req.session && req.session.user) {
@@ -18,7 +12,6 @@ async function checkSession(req, res, next) {
 
 router.get('/', checkSession, (req, res, next) => {
     return res.json({userId: req.session.user});
->>>>>>> d76d084300729185a092b9bcdc82cae7ba022965
 })
 
 module.exports = router;
